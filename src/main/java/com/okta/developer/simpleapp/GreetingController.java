c0a6e3aaca138c77d442b8b5d51b9ac346a1de56
wallxwallxz1 <wallxwallxz1@gmail.com>
2020-05-24 12:33:33 -0500

    initial commit

---
+++ src/main/java/com/okta/developer/simpleapp/GreetingController.java
@@ -0,0 +1,21 @@
+package com.okta.developer.simpleapp;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greet(@AuthenticationPrincipal OidcUser user){
+
+        return "Hello  " +user.getEmail();
+    }
+
+
+}
+
+
